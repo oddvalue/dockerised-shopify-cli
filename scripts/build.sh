29f29ac2eb6b2a0138d1a4cc47ef533c2707e9f1
jim <jim@oddvalue.co.uk>
2022-03-18 09:42:27 +0000

    Remove bad docker build arg

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build --no-cache --force-rebuild --build-arg UID=$(id -u) --build-arg GID=$(id -g) ./ -t shopify-cli:latest
+docker build --no-cache --build-arg UID=$(id -u) --build-arg GID=$(id -g) ./ -t shopify-cli:latest
